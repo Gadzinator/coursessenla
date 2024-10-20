9ea5cd47cb3b90cd5f97781f063c21b8bb256cff
grigorev <ilovemeet2022@gmail.com>
2024-10-20 18:40:55 +0300

    Deleted annotation Transaction.java and JsonConversionException.java.

--- src/main/java/com/coursessenla/main/exception/JsonConversionException.java
+++
@@ -1,8 +0,0 @@
-package com.coursessenla.main.exception;
-
-public class JsonConversionException extends RuntimeException {
-
-	public JsonConversionException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
