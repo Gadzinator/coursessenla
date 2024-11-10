b16a664b3c1155ccfa5276af53bf0ad672ab710f
grigorev <ilovemeet2022@gmail.com>
2024-11-10 16:06:50 +0300

    Refactoring

--- src/main/java/com/coursessenla/main/service/impl/ActorServiceImpl.java
+++ src/main/java/com/coursessenla/main/service/impl/ActorServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
--- src/main/java/com/coursessenla/main/service/impl/AdminServiceImpl.java
+++ src/main/java/com/coursessenla/main/service/impl/AdminServiceImpl.java
@@ -17,9 +17,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
